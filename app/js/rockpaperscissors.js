93876f38b63b38708aa153aa8e6a009b31f67578
Matt Rosenberg <mattnbella@Matts-MacBook-Pro.local>
2015-09-12 18:24:51 -0700

    added 2 lines of code

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,17 +22,25 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
+    
+var move;
+move = move || 'getInput()';
+
+// Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return move = move || 'getInput()';
 }
 
 function getComputerMove(move) {
+
+var move;
+move = move || 'randomPlay()';
+
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move = move || 'randomPlay()';
 }
 
 function getWinner(playerMove,computerMove) {
