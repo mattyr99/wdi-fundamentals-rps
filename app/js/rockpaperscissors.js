786d10ccf0bc8f53a27ef45ef6e823e08fa10fbf
Matt Rosenberg <mattnbella@Matts-MacBook-Pro.local>
2015-09-22 14:42:22 -0700

    added code for unit 4 or games rules with if statements

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -45,6 +45,23 @@ move = move || 'randomPlay()';
 
 function getWinner(playerMove,computerMove) {
     var winner;
+}
+if (playerMove === computerMove) {
+return “tie”;
+} else if (playerMove === “rock” && computerMove === “scissors”) {
+return 'Player Wins!';
+} else if (playerMove === “paper” && computerMove === “rock”) {
+return 'Player Wins!';
+} else if (playerMove === “scissors” && computerMove === “paper”) {
+return 'Player Wins!';
+} else if (computerMove === “rock” && playerMove === “scissors”) {
+return 'Player Wins!';
+} else if (computerMove === “paper” && playerMove === “rock”) {
+return 'Player Wins!';
+} else if (computerMove === “scissors” && playerMove === “paper”) {
+return 'Player Wins!';
+}
+
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
